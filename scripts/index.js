e999f6a0db500ed663f3d245528d75f9ce39faca
JoelTapia99 <joelts159@gmail.com>
2019-12-11 00:48:20 -0500

    aagredo ejemplos de navs y mejora visual en index

--- scripts/index.js
+++ scripts/index.js
@@ -2,3 +2,15 @@ function validarSiNumero(numero){
     if (!/^([0-9])*$/.test(numero))
       alert("El valor " + numero + " no es un número");
   }
+
+function primerForm(){
+  document.getElementById("primerEstilo").style.display="block";
+  document.getElementById("segundoEstilo").style.display="none";
+  
+}
+
+function segundoForm(){
+  document.getElementById("primerEstilo").style.display="none";
+  document.getElementById("segundoEstilo").style.display="block";
+
+}
