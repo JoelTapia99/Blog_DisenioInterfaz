0f6baf96b4bfebf888696d97a527b8239eb34b22
JoelTapia99 <joelts159@gmail.com>
2019-12-11 21:50:52 -0500

    agrego tercer fotmulario

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,26 @@ function validarSiNumero(numero){
       alert("El valor " + numero + " no es un número");
   }
 
-function primerForm(){
-  document.getElementById("primerEstilo").style.display="block";
-  document.getElementById("segundoEstilo").style.display="none";
+  function primerForm(){
+    document.getElementById("primerEstilo").style.display="block";
+    document.getElementById("segundoEstilo").style.display="none";
+    
+  }
   
-}
-
-function segundoForm(){
-  document.getElementById("primerEstilo").style.display="none";
-  document.getElementById("segundoEstilo").style.display="block";
-
-}
+  function segundoForm(){
+    document.getElementById("primerEstilo").style.display="none";
+    document.getElementById("segundoEstilo").style.display="block";
+  
+  }
+  
+  function redactarCorreos(){
+    document.getElementById("redactarCorreos").style.display="block";
+    document.getElementById("crearGrupos").style.display="none";
+  
+  }
+  
+  function crearGrupos(){
+    document.getElementById("redactarCorreos").style.display="none";
+    document.getElementById("crearGrupos").style.display="block";
+  
+  }
